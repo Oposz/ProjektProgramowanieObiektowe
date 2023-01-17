4419d5aa2c44fa249e32f593c97f278270f82b03
Przemek Gulik <gulikdev@gmail.com>
2023-01-17 17:31:27 +0100

    Initial commit

---
+++ src/main/java/com/example/programowanieobiektoweprojekt/ProgramowanieObiektoweProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.programowanieobiektoweprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramowanieObiektoweProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramowanieObiektoweProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/programowanieobiektoweprojekt/ProgramowanieObiektoweProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.programowanieobiektoweprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramowanieObiektoweProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
